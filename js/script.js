554b49fd7af108de5c7f1457d8e4cf7a09560fb7
Oritsejolomi Sillo <jsillo38@gmail.com>
2020-03-19 23:55:08 +0000

    ‘commit’

--- js/script.js
+++ js/script.js
@@ -4,9 +4,24 @@ var hotel = {
     rooms: 110,
     booked: 25,
     gym: true,
+    pool: true,
     roomTypes: ['double', 'twin', 'suite', 'king'],
     checkAvailablity: function() {
         return this.rooms - this.booked;
+    },
+    checkPool: function() {
+        if(this.pool){
+            return 'Pool Avialable';
+        }else{
+            return 'Pool Unavialable';
+        }
+    },
+    checkGym: function() {
+        if(this.gym){
+            return 'Gym Avialable'
+        }else{
+            return 'Gym Unavialable'
+        }
     }
 };
 
@@ -15,4 +30,10 @@ elName.textContent = hotel.name;
 
 var elRooms = document.getElementById('rooms');
 elRooms.textContent = hotel.checkAvailablity();
+
+var elPool = document.getElementById('pool');      
+elPool.textContent = hotel.checkPool;                     
+
+var elGym = document.getElementById('gym');        
+elGym.textContent = hotel.checkGym;
 
